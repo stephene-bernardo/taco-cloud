18299acade67e5ed448d707f5dbe4ac9b6b14084
Karl Bernardo <stephene.bernardo@gmail.com>
2021-07-10 19:39:59 +0800

    added configurations

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import tacos.data.IngredientRepository;
 
@@ -14,10 +15,12 @@ public class TacoCloudApplication {
 	}
 
 	@Bean
+	@Profile("!prod")
 	public CommandLineRunner dataLoader(IngredientRepository repo) {
 		return new CommandLineRunner() {
 			@Override
 			public void run(String... args) throws Exception {
+				System.out.println("***thiswork");
 				repo.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
 				repo.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
 				repo.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
